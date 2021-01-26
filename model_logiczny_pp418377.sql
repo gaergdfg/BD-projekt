80e521b4d33ebad536d8e1466ea9dc4f92548f71
gaergdfg <39438240+gaergdfg@users.noreply.github.com>
2021-01-26 15:01:27 +0100

    init

---
+++ model_logiczny_pp418377.sql
@@ -0,0 +1,108 @@
+drop table obecnie_wypozyczone cascade constraints;
+drop table historia_wypozyczen cascade constraints;
+drop table czytelnik cascade constraints;
+drop table ksiazka cascade constraints;
+drop table biblioteka cascade constraints;
+drop table miasto cascade constraints;
+
+create table miasto(
+	id_miasta int not null primary key,
+	nazwa varchar2(20) not null
+);
+
+create table biblioteka(
+	id_biblioteki int not null primary key,
+	nazwa varchar2(50) not null,
+	id_miasta int references miasto(id_miasta)
+);
+
+create table ksiazka(
+	id_ksiazki int not null primary key,
+	tytul varchar2(50) not null,
+	liczba number(2) not null,
+	przedzial int not null,
+	id_biblioteki int references biblioteka(id_biblioteki)
+);
+
+create table czytelnik(
+	id_czytelnika int not null primary key,
+	imie varchar2(20) not null,
+	nazwisko varchar2(20) not null
+);
+
+create table historia_wypozyczen(
+	id_czytelnika int not null references czytelnik,
+	id_ksiazki int not null references ksiazka
+);
+
+create table obecnie_wypozyczone(
+	id_czytelnika int not null references czytelnik,
+	id_ksiazki int not null references ksiazka
+);
+
+
+-- ============================== UPDATE ==============================
+create or replace trigger validate_ksiazka_update
+before update
+on ksiazka
+for each row
+begin
+	if :NEW.liczba - :OLD.liczba <> 1 then
+		raise_application_error(-20000, 'Proba wypozyczenia wiecej niz jednej ksiazki na raz');
+	end if;
+end;
+/
+
+create or replace trigger validate_historia_update
+before update
+on historia_wypozyczen
+for each row
+begin
+	raise_application_error(-20000, 'Proba zmiany danych na temat wypozyczonych ksiazek');
+end;
+/
+
+create or replace trigger validate_obecne_update
+before update
+on obecnie_wypozyczone
+for each row
+begin
+	raise_application_error(-20000, 'Proba zmiany danych na temat obecnie wypozyczonych ksiazek');
+end;
+/
+
+
+-- ============================== REMOVE ==============================
+create or replace trigger validate_miasto_remove
+before delete
+on miasto
+declare
+begin
+	raise_application_error(-20000, 'Proba usuniecia miasta z bazy danych');
+end;
+/
+
+create or replace trigger validate_biblioteka_remove
+before delete
+on biblioteka
+declare
+begin
+	raise_application_error(-20000, 'Proba usuniecia biblioteki z bazy danych');
+end;
+/
+
+create or replace trigger validate_ksiazka_remove
+before delete
+on ksiazka
+for each row
+declare
+	liczba_wypozyczonych number;
+begin
+	select count(*) into liczba_wypozyczonych from obecnie_wypozyczone where obecnie_wypozyczone.id_ksiazki = :OLD.id_ksiazki;
+
+	if liczba_wypozyczonych <> 0 then
+		raise_application_error(-20000, 'Proba usuniecia ksiazki, ktorej egzemplarze sa obecnie wypozyczone');
+	end if;
+end;
+/
+
---
+++ parser.py
@@ -0,0 +1,90 @@
+import re
+from random import randint as rand
+
+
+# Parsing data
+
+with open("books.txt", encoding = "utf-8") as file:
+	data = file.readlines()
+
+target_audience = 0
+books = []
+
+for line in data:
+	if line == "\n":
+		continue
+
+	line = line[:-1]
+
+	if line[-1] == ':':
+		target_audience = target_audience + 1
+		continue
+
+	line_data = re.split(" – ", line)
+	author = line_data[0]
+	titles = re.split("; ", line_data[1])
+
+	for title in titles:
+		books.append((author, title, target_audience))
+
+cities = []
+with open("cities.txt", "r", encoding = "utf-8") as file:
+	for line in file:
+		line = line[:-1]
+		if (line != ""):
+			cities.append(re.split("\t", line))
+
+libraries = []
+with open("libraries.txt", "r", encoding = "utf-8") as file:
+	for line in file:
+		line = line[:-1]
+		if (line != ""):
+			libraries.append(re.split("\t", line))
+
+
+# Creating sql script
+
+with open("create_library_assets.sql", "w", encoding = "utf-8") as file_1, open("model_logiczny_pp418377.sql", "r", encoding = "utf-8") as file_2:
+	for line in file_2:
+		file_1.write(line)
+
+with open("create_library_assets.sql", "a", encoding = "utf-8") as file:
+	table_name = "miasto"
+	file.write("\n-- ============================== MIASTO ==============================\n\n")
+	for city in cities:
+		file.write("insert into {0} values ({1}, \"{2}\");\n".format(table_name, city[0], city[1]))
+
+	table_name = "biblioteka"
+	last_city_id = '10'
+	file.write("\n\n-- ============================== BIBLIOTEKA ==============================\n\n")
+	for library in libraries:
+		if last_city_id != library[2]:
+			last_city_id = str(int(last_city_id) + 1)
+			file.write("\n")
+
+		file.write("insert into {0} values ({1}, \"Biblioteka {2}\", {3});\n".format(table_name, library[0], library[1], library[2]))
+
+	assignments = {}
+	table_name = "ksiazka"
+	file.write("\n\n-- ============================== KSIAZKA ==============================\n\n")
+	last_target = 1
+	for book in books:
+		if last_target != book[2]:
+			last_target = last_target + 1
+			file.write("\n")
+
+		library_index = rand(0, len(libraries) - 1)
+		if library_index in assignments:
+			assignments[library_index] = assignments[library_index] + 1
+		else:
+			assignments[library_index] = 1
+
+		file.write(
+			"insert into {0} values (\"{1}\", \"{2}\", {3}, {4}, {5});\n"
+			.format(table_name, book[0], book[1], rand(2, 5), book[2], libraries[library_index][0])
+		)
+
+	minmax = (420, -1)
+	for val in assignments.values():
+		minmax = (min(minmax[0], val), max(minmax[1], val))
+	print(minmax)
