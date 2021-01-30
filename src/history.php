e8461c997e22ec3bbbc7b5bf1aaeb34465add575
Piotr Prabucki <pp418377@students.mimuw.edu.pl>
2021-01-30 15:19:34 +0100

    add history query

--- src/history.php
+++ src/history.php
@@ -2,7 +2,6 @@
 	<head>
 		<title>Serwis Biblioteczny - Historia</title>
 		<meta http-equiv="content-type" content="text/html" charset="utf-8">
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script>
 	</head>
 
 	<body>
@@ -20,6 +19,29 @@
 			}
 
 			$login = $_SESSION['LOGIN'];
+
+			$query = oci_parse($connection, "select id_czytelnika from czytelnik where login = :login_");
+			oci_bind_by_name($query, ":login_", $login);
+			oci_execute($query);
+			$row = oci_fetch_array($query, OCI_BOTH);
+			$id = $row['ID_CZYTELNIKA'];
+
+			$query = oci_parse($connection, "select ksiazka.tytul as tytul, ksiazka.autor as autor, ksiaska.przedzial as przedzial from historia_wypozyczen, ksiazka where historia_wypozyczen.id_czytelnika = :id_ and historia_wypozyczen.id_ksiazki = ksiazka.id_ksiazki");
+			oci_bind_by_name($query, ":id_", $id);
+			oci_execute($query);
+
+			$is_empty = true;
+			echo "Twoja historia wypozyczen:<br><br>";
+			echo "<ul>";
+			while ($row = oci_fetch_array($query, OCI_BOTH)) {
+				$is_empty = false;
+				echo "<li>", "Tytul: ", $row['TYTUL'], "<br>Autor: ", $row['AUTOR'], "<br>Poziom czytelniczy: ", $row['PRZEDZIAL'], "</li>";
+			}
+			echo "</ul>";
+
+			if ($is_empty) {
+				echo "<br><p>Wyglada na to, ze nie masz jeszcze wypozyczyles(as) jeszcze zadnej ksiazki.</p>";
+			}
 		?>
 	</body>
 </html>
